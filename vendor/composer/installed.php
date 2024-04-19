86d9b6fba8c00fc81eba3c33587334ee46ab4914
Ivan <ivanmarmi@hotmail.es>
2024-04-19 20:07:37 +0200

    1 - CETI - First html
    - Created first html

---
+++ vendor/composer/installed.php
@@ -0,0 +1,38 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ivanm/eshop-pps',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '180d701c7208546119cc118b0c630cf07d2b9476',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ivanm/eshop-pps' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '180d701c7208546119cc118b0c630cf07d2b9476',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twbs/bootstrap' => array(
+            'pretty_version' => 'v5.3.3',
+            'version' => '5.3.3.0',
+            'reference' => '6e1f75f420f68e1d52733b8e407fc7c3766c9dba',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twbs/bootstrap',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twitter/bootstrap' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => 'v5.3.3',
+            ),
+        ),
+    ),
+);
