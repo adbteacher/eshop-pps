9735451b685668a551ab5abdd3a57bcb4ea06758
Rubén García Serrano <rubengarciaserrano88@gmail.com>
2024-05-24 19:32:50 +0200

    environment changes and default values

--- mail_config.php
+++ mail_config.php
@@ -14,15 +14,15 @@ function getMailer(): PHPMailer
 
     //Server settings
     $mail->isSMTP();
-    $mail->Host       = 'smtp.example.com';  // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;               // Enable SMTP authentication
-    $mail->Username   = 'user@example.com'; // SMTP username
-    $mail->Password   = 'secret';           // SMTP password
+    $mail->Host       = getenv('MAIL_HOST');      // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                     // Enable SMTP authentication
+    $mail->Username   = getenv('MAIL_USERNAME');  // SMTP username
+    $mail->Password   = getenv('MAIL_PASSWORD');  // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption
-    $mail->Port       = 587;                // TCP port to connect to
+    $mail->Port       = 587;                      // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('no-reply@example.com', 'Mailer');
+    $mail->setFrom(getenv('MAIL_FROM'), getenv('MAIL_FROM_NAME'));
 
     return $mail;
 }
