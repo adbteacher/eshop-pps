e75b4b52ccb41e7f0f0a4d4e026f7e28db2b386b
josevi-rca <166542222+josevi-rca@users.noreply.github.com>
2024-06-07 17:54:55 +0200

    Correción de errores

--- 3register/js/main.js
+++ 3register/js/main.js
@@ -5,31 +5,51 @@
  *
  */
 
-jQuery(document).ready(function ($)
-{
-	$('input[name="UserType_U"]').attr('checked', 'checked');
+jQuery(document).ready(function ($) {
+    $('input[name="UserType_U"]').attr('checked', 'checked');
 
-	$('li.hidden > input').removeAttr('required');
-	$('input[name="UserType"').change(function () {
-		$('li[data-form="U"').toggleClass('hidden');
-		$('li[data-form="V"').toggleClass('hidden');
-		$('input[data-required="true"]').attr('required', 'true');
-		$('li.hidden > input').removeAttr('required');
-	});
+    $('li.hidden > input').removeAttr('required');
+    $('input[name="UserType"]').change(function () {
+        var userType = $(this).val();
+        $('li[data-form="U"]').each(function() {
+            $(this).addClass('fadeOut');
+            setTimeout(() => {
+                $(this).addClass('hidden').removeClass('fadeOut').removeClass('fadeIn');
+                if (userType === 'U') {
+                    $(this).removeClass('hidden').addClass('fadeIn');
+                }
+                $(this).find('input').attr('required', userType === 'U');
+            }, 500);
+        });
+        $('li[data-form="V"]').each(function() {
+            $(this).addClass('fadeOut');
+            setTimeout(() => {
+                $(this).addClass('hidden').removeClass('fadeOut').removeClass('fadeIn');
+                if (userType === 'V') {
+                    $(this).removeClass('hidden').addClass('fadeIn');
+                }
+                $(this).find('input').attr('required', userType === 'V');
+            }, 500);
+        });
+    });
 
-	$('form').on('click', 'input[name="register"]', function(e){
-		var error = false;
-		$('input[data-required="true"]').each(function(){
-			var msgError = 'div.' + $(this).attr('id') + 'Error';
-			if(!$(msgError).hasClass('hidden')){ $(msgError).addClass('hidden'); }
-			if($(this).val().trim() == '' && !$(this).parent().hasClass('hidden')){
-				$(msgError).removeClass('hidden')
-				error = true;
-			}
-		});
-		if(error){
-			e.preventDefault();
-			return false;
-		}
-	});
+    $('form').on('click', 'input[name="register"]', function (e) {
+        var error = false;
+        $('input[data-required="true"]').each(function () {
+            var msgError = 'div.' + $(this).attr('id') + 'Error';
+            if (!$(msgError).hasClass('hidden')) { $(msgError).addClass('hidden'); }
+            if ($(this).val().trim() == '' && !$(this).parent().hasClass('hidden')) {
+                $(msgError).removeClass('hidden')
+                error = true;
+            }
+        });
+        if (error) {
+            e.preventDefault();
+            return false;
+        }
+    });
 });
+
+
+
+
